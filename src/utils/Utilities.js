4d9c6f9ed6a6c0228e35cb65e1440f8531d4fc1a
HuyHt <huyht5@vng.com.vn>
2023-02-05 16:32:04 +0700

    add IPFS function

--- src/utils/Utilities.js
+++ src/utils/Utilities.js
@@ -1,14 +1,37 @@
 
+import * as IPFS from 'ipfs-core';
+import {Buffer} from 'buffer'; 
+// const node_ipfs = await IPFS.create();
+var node_ipfs = null;
+async function _getNodeIPFS()
+{
+    if(node_ipfs == null) node_ipfs = await IPFS.create();
+    // console.log(node_ipfs);
+    return node_ipfs;
+    // if(node_ipfs == null) node_ipfs = await IPFS.create();
+    // return node_ipfs;
+}
 export function isLogedIn()
 {
     // todo: check function
     let accounts = [];
-    if (localStorage.getItem("accounts") != null)
+    if (sessionStorage.getItem("accounts") != null)
     {
-        accounts = JSON.parse(localStorage.getItem("accounts"));
+        accounts = JSON.parse(sessionStorage.getItem("accounts"));
     }
     console.log("connected accounts: "+ JSON.stringify(accounts));
     return accounts.length>0;
+}
+
+export function getAccounts()
+{
+    let accounts = [];
+    if (sessionStorage.getItem("accounts") != null)
+    {
+        accounts = JSON.parse(sessionStorage.getItem(accounts));
+    }
+    if(accounts == null) accounts = [];
+    return accounts;
 }
 
 export function log(...str)
@@ -23,8 +46,6 @@ export function log(...str)
 export function isMetaMaskInstalled()
 {
     const {ethereum} = window;
-    // console.log(window);
-    // console.log(window.ethereum);
     if(ethereum && ethereum.isMetaMask)
     {
         return true;
@@ -35,13 +56,67 @@ export function isLoading(){
     return (
         <div classname="Loading">
             hello
-            <img
-            classname = "logo"
-            src ="logo192.png"
-            ></img> 
-            <h1 className = "title">
-            hello xin chào bạn
-            </h1>
-            </div>
+        </div>
     )
 }
+
+export function makeIPFSData({title, content, num_medals, owner})
+{
+    let data = {
+        title: title,
+        content: content,
+        num_medals: num_medals,
+        owner: owner
+    }
+
+    return encodeData(JSON.stringify(data));
+}
+
+export function encodeData(data)
+{
+    return data;
+}
+
+export function decodeData(encodedData)
+{
+    return encodedData;
+}
+
+export async function addFileToIPFS(filePath, content)
+{
+    // console.log('adding a file to IPFS: '+ JSON.stringify(content));
+    // console.log(await _getNodeIPFS());
+    let node = await _getNodeIPFS();
+    let options = {
+        warpWithDirectory: true
+    }
+    let result = await node.add(
+        {
+            path: filePath,
+            content: content
+        },
+        options
+    );
+    // let str = new TextDecoder().decode(result.cid.bytes)
+    // console.log(str);
+    // console.log(result.cid.toString());
+    // console.log(result);
+    // let res = await retrieveConternFromIPFS(result.cid.toString()) ;
+    // console.log(res);
+    return  result;
+}
+
+export async function retrieveConternFromIPFS(hashAddress)
+{
+    let data = "";
+    let node = await _getNodeIPFS();
+    for await(let chunk of node.cat(hashAddress))
+    {
+        data = data + Buffer.from(chunk).toString();
+        // chunks.push(chunk);
+    }
+    // let test = await node.cat(hashAddress);
+    // console.log(data);
+    return JSON.parse(decodeData(data));
+}
+
